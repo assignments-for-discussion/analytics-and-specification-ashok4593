f8aaf1b3d5c32cccae8c078dd5bbeba963069369
ashok4593 <ashok.gami@somaiya.edu>
2021-12-16 11:48:31 +0530

    average modified, returns NaN when most of the input-numbers are NaN

--- average.js
+++ average.js
@@ -1,14 +1,21 @@
 function average(numbers) {
   let totalNotNulls = numbers.length;
-  return (
-    numbers.reduce((p, c) => {
-      if (c) {
-        return p + c;
-      }
-      totalNotNulls--;
-      return p;
-    }, 0) / totalNotNulls
-  );
+  // failure tolerance can be considered as a threshold, if number of NaNs goes beyond a threshold we can say sensor is unreliable.
+  // failure tolerance value ranges between 0 and 1.
+  //eg:- 0.5 indicates if sensor failes to capture 50% of values we say that sensor is unreliable
+  let failureTolerance = 0.5;
+
+  const sumOfReadings = numbers.reduce((p, c) => {
+    if (c) {
+      return p + c;
+    }
+    totalNotNulls--;
+    return p;
+  }, 0);
+
+  if ((totalNotNulls / numbers.length).toFixed(2) <= failureTolerance)
+    return NaN;
+  return sumOfReadings / totalNotNulls;
 }
 
 module.exports = { average };
